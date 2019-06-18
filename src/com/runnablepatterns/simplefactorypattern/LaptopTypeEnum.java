35d883d3664d71c81271fa9993cee38d6b762fde
Carlos Marin <carmartr06@gmail.com>
2019-06-18 00:26:18 -0400

    Fix comment typo

--- src/com/runnablepatterns/simplefactorypattern/LaptopTypeEnum.java
+++ src/com/runnablepatterns/simplefactorypattern/LaptopTypeEnum.java
@@ -5,7 +5,7 @@ package com.runnablepatterns.simplefactorypattern;
  * @author Carlos Marin
  * Runnable Patterns (runnablepatterns.com)
  * 
- * Class to handle different Laptop types for instanciation
+ * Class to handle different Laptop types for instantiation
  *
  */
 public enum LaptopTypeEnum {
